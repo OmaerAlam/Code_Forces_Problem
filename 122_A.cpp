039a6b7bcb7a85a7978637375b2fafeca0dab608
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2023-02-22 01:32:53 +0600

    A. Lucky Division

---
+++ 122_A.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+int main(){
+    optimize();
+   int t;
+   cin>>t;
+   if(t%4==0|| t%7==0|| t%44==0|| t%47==0|| t%74==0|| t%77==0|| t%444==0|| t%474==0|| t%477==0|| t%744==0|| t%774==0|| t%777==0) cout<<"YES"<<endl;
+   else cout<<"NO"<<endl;
+return 0;
+}
